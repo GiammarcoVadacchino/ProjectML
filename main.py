7cb376664fe0ce740f38310611112fca9a7b59f7
giammarcovadacchino <giammarco.vadacchino@studenti.unimi.it>
2026-05-07 16:57:37 +0200

    add comments

--- main.py
+++ main.py
@@ -1,107 +1,137 @@
-
-from sklearn.datasets import make_moons
 from train import *
 from utils import *
 import numpy as np
 from plot import *
-
+from sklearn.datasets import make_classification
 
 SEED = 42
 np.random.seed(SEED)
 
 
+RUN_SCALABILITY_EXPERIMENT = False #flag
+USE_REAL_DATASET = True #switch if you want to use synthetic dataset
+
 def main():
 
-    sizes = [500, 1000, 2000, 4000, 8000, 15000]
+    #params for multiple run experiments of random features
+    n_runs = 5
+    seeds = [SEED + i for i in range(n_runs)]
 
-    #TODO: whene i finshed the pipeline with synthetic dataset, rerune the pipeline with real world data
-    #NOTE: 5k samples for exps
-    X, y = make_moons(n_samples=5000,noise=0.8,random_state=SEED)
 
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,seed=SEED)
+    if USE_REAL_DATASET:
+        X, y = get_real_dataset(n_samples=2500, seed=SEED)
+    else:
+        #crate synthetic non linear dataset
+        X, y = make_classification(
+            n_samples= 2500,
+            n_features=10,
+            n_informative=4,
+            n_redundant=3,
+            flip_y=0.05,
+            class_sep=0.8,
+            random_state=SEED
+        )
 
+    sizes = [500, 1000, 2500, 4000, 8000, 15000]
+
+    #Train and test split (70/30)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, seed=SEED)
     X_train, X_test = standardize(X_train, X_test)
 
-    D_rff_values = [25, 50, 100, 250, 500, 1000, 2000]
-    D_binning_values = [10, 25, 50, 100, 250, 500] #NOTE: explain this
-    gamma_values = np.logspace(-2, 1, 6) # need to be tuned
-    C_values = np.logspace(-2, 2, 6) # need to be tuned
+    D_rff_values = [25, 50, 100, 250, 500, 1000, 2000, 5000, 10000, 20000] # d values for RFF
+    D_binning_values = [10, 25, 50, 100, 250, 500, 1000, 2000] # da values for RB (the matrix is then d * buckets, this explain the fact that i use smaller values of d)
+    gamma_values = np.logspace(-2, 1, 6) 
+    C_values = np.logspace(-2, 2, 6)
 
-    #RUN PERCEPTRON LINEAR save train error, test error, accuracy, time
 
-    p_linear_results = train_evaluate_perceptron(X_train,y_train,X_test,y_test,best_gamma=None,feature_map_name="linear",D_values=None)
+    #run linear perceptron
+    p_linear_results = train_evaluate_perceptron(X_train, y_train, X_test, y_test, best_gamma=None, feature_map_name="linear", D_values=None)
     print("\n===== PERCEPTRON LINEARE =====")
     print_stats(p_linear_results)
 
-
-    #RUN SVM RBF (5-fold-cv) save train error, test error, accuracy, time and gamma values
-    svm_rbf_results = train_evaluate_svm(X_train,y_train,X_test,y_test,C_values,gamma_values, kernel = "rbf")
+    #run SVM with RBF kernel
+    svm_rbf_results = train_evaluate_svm(X_train, y_train, X_test, y_test, C_values, gamma_values, kernel="rbf")
+    print("\n===== SVM RBF =====")
     print_stats(svm_rbf_results)
 
-    #RUN SVM LAPLACE (5-fold-cv) save train error, test error, accuracy, time and gamma values
-    svm_laplace_results = train_evaluate_svm(X_train,y_train,X_test,y_test,C_values,gamma_values, kernel = "laplace")
+    #run SVM with Laplace kernel
+    svm_laplace_results = train_evaluate_svm(X_train, y_train, X_test, y_test, C_values, gamma_values, kernel="laplace")
+    print("\n===== SVM LAPLACE =====")
     print_stats(svm_laplace_results)
 
-
-    best_gamma_rbf = svm_rbf_results["rbf"][3] #NOTE: best gamma founed in CV with rbf kernel has to be used in perceptron with RFF
-    best_gamma_laplace = svm_laplace_results["laplace"][3] #NOTE: best gamma founed in CV with laplace kernel has to be used in perceptron with random binning features
-
-
-    #RUN PERCEPTRON RFF save train error, test error, accuracy, time and D values
-    p_rff_results = train_evaluate_perceptron(X_train,y_train,X_test,y_test,best_gamma_rbf,"rff",D_rff_values)
-    print("\n===== PERCEPTRON RFF =====")
-    print_stats(p_rff_results)
-
-    #RUN PERCEPTRON BINNING, save train error, test error, accuracy, time and D values
-    p_random_binning_results = train_evaluate_perceptron(X_train,y_train,X_test,y_test,best_gamma_laplace,"random_binning",D_binning_values)
-    print("\n===== PERCEPTRON RANDOM BINNING =====")
-    print_stats(p_random_binning_results)
+    #take the best gammas after 5-fold-cross-validation
+    best_gamma_rbf = svm_rbf_results["rbf"][3]
+    best_gamma_laplace = svm_laplace_results["laplace"][3]
 
 
-    #PLOTS and STORE
+
+    all_p_rff_results = []
+    all_p_random_binning_results = []
+
+    #multiple runs for random features
+    for run_id, seed in enumerate(seeds):
+
+        print(f"\n\n========== RUN {run_id + 1}/{n_runs} - SEED {seed} ==========")
+
+        np.random.seed(seed)
+
+        #run Random Fourier Features
+        p_rff_results_run = train_evaluate_perceptron(X_train, y_train, X_test, y_test, best_gamma_rbf, "rff", D_rff_values)
+        print("\n===== PERCEPTRON RFF - SINGLE RUN =====")
+        print_stats(p_rff_results_run)
+        all_p_rff_results.append(p_rff_results_run)
+
+        np.random.seed(seed)
+
+        #run Random Binning Features
+        p_random_binning_results_run = train_evaluate_perceptron(X_train, y_train, X_test, y_test, best_gamma_laplace, "random_binning", D_binning_values)
+        print("\n===== PERCEPTRON RANDOM BINNING - SINGLE RUN =====")
+        print_stats(p_random_binning_results_run)
+        all_p_random_binning_results.append(p_random_binning_results_run)
 
 
-    #Insight into representation vs computation trade-offs
-    #TODO: plot test error vs number of features (for rff)
-    #TODO: plot test error vs number of features (for binning)
-    plot_test_error_vs_features(p_rff_results, label = "Perceptron RFF",save_path='plots/test_error_vs_features_rff')
-    plot_test_error_vs_features(p_random_binning_results, label = "Perceptron RB",save_path='plots/test_error_vs_features_rb')
+    #aggregate and print results (mean +- std)
+    p_rff_results, p_rff_std = aggregate_results(all_p_rff_results)
+    p_random_binning_results, p_random_binning_std = aggregate_results(all_p_random_binning_results)
 
-    
-
-    #Evidence that random features approximate kernel performance, Comparison between linear, approximate kernel, and exact kernel models, Compare different feature map constructions
-    #TODO: comparison with exact kernel method, (comparison between, train/test error, of svm_rbf, perceptron, percetron rff)
-    plot_kernel_comparison(svm_rbf_results,p_linear_results,p_rff_results,kernel="rbf",save_path="plots/kernel_compariso_rbf_rff")
-    #TODO: comparison with exact kernel method, (comparison between, train/test error, of svm_laplace, perceptron, percetron binning)
-    plot_kernel_comparison(svm_laplace_results,p_linear_results,p_random_binning_results,kernel="laplace",approx_label="RB",save_path="plots/kernel_compariso_laplace_rb")
+    print_stats_mean_std(p_rff_results, p_rff_std, title="PERCEPTRON RFF")
+    print_stats_mean_std(p_random_binning_results, p_random_binning_std, title="PERCEPTRON RANDOM BINNING")
 
 
-    #Analyze runtime vs accuracy trade-off
-    #TODO: plot runtime vs accuracy (x training time, y acc) NO CV (all models so 5 lines) 2 subplots, one with training and one with test error
-    plot_runtime_vs_accuracy(svm_rbf_results,p_linear_results,p_rff_results,kernel="rbf",save_path="plots/runtime_vs_accuracy_rbf_rff")
-     #TODO: plot runtime vs accuracy (x total time, y acc) WITH CV (all models so 5 lines)
-    plot_runtime_vs_accuracy(svm_laplace_results,p_linear_results,p_random_binning_results,kernel="laplace",approx_label="RB",save_path="plots/runtime_vs_accuracy_laplace_rb")
-   
 
-    #Scalabily of perceptron with featrues maps w.r.t SVM 
-    #TODO: plot runtime vs dataset size (prove scalability of features transformations)
+    real = "" #flag to distinguish if i use real or synthetic data
+    if USE_REAL_DATASET: real = "real"
 
-    """
-    asse x = dataset size (n)
-    asse y = training time
-    curve:
-    SVM rbf
-    SVM laplace
-    Perceptron + RFF
-    Perceptron + RB
-    """
+    plot_test_error_vs_features(p_rff_results,label="Perceptron RFF", save_path=f"plots/test_error_vs_features_rff_{real}")
+    plot_test_error_vs_features(p_random_binning_results, label="Perceptron RB", save_path=f"plots/test_error_vs_features_rb_{real}")
 
-    #Study effect of kernel bandwidth
-    #TODO: plot SVM rbf and perceptron with RFF (x = gamma (log scale), y = train/test error)
+    plot_kernel_comparison(svm_rbf_results, p_linear_results, p_rff_results, kernel="rbf", save_path=f"plots/kernel_compariso_rbf_rff_{real}")
+    plot_kernel_comparison(svm_laplace_results, p_linear_results, p_random_binning_results, kernel="laplace", approx_label="RB", save_path=f"plots/kernel_compariso_laplace_rb_{real}")
+
+    plot_runtime_vs_accuracy(svm_rbf_results, p_linear_results, p_rff_results, kernel="rbf", save_path=f"plots/runtime_vs_accuracy_rbf_rff_{real}")
+    plot_runtime_vs_accuracy(svm_laplace_results, p_linear_results, p_random_binning_results, kernel="laplace", approx_label="RB", save_path=f"plots/runtime_vs_accuracy_laplace_rb_{real}")
+
     gammas = np.logspace(-3, 1, 8)
-    study_kernel_bandwidth(X_train, y_train, X_test, y_test,gammas,D=200,svm_kernel="rbf",save_path="plots/bandwidth_rbf.png")
-    study_kernel_bandwidth(X_train, y_train, X_test, y_test,gammas,D=200,svm_kernel="laplace",save_path="plots/bandwidth_laplace.png")
 
+    study_kernel_bandwidth(X_train, y_train, X_test, y_test, gammas, D=200, svm_kernel="rbf", save_path=f"plots/bandwidth_rbf_{real}")
+    study_kernel_bandwidth(X_train, y_train, X_test, y_test, gammas, D=200, svm_kernel="laplace", save_path=f"plots/bandwidth_laplace_{real}")
 
+    if RUN_SCALABILITY_EXPERIMENT:
+
+        scalability_results = run_scalability_experiment(
+            sizes=sizes,
+            C_values=C_values,
+            gamma_values=gamma_values,
+            best_gamma_rbf=best_gamma_rbf,
+            best_gamma_laplace=best_gamma_laplace,
+            D_rff=500,
+            D_binning=100
+        )
+
+        plot_scalability_results(
+            scalability_results,
+            save_path="plots/scalability_training_time"
+        )
+    
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -4,16 +4,26 @@ import kernels
 
 class RandomFourierFeatures:
     def __init__(self, D=100, gamma=1.0):
-        self.D = D
-        self.gamma = gamma
+        self.D = D #number of finite dimensions
+        self.gamma = gamma #Parameter of the RBF kernel, control the velocity of decrase of similarity among points
+
+        #RBF kernels, points closer in the original space are similar in the new transformated space
 
     def fit(self, X):
+        #get number of fetaures
         d = X.shape[1]
+
+        #Build the W matrix, where each row is a random dircetion in the space
+        #the number are sampled from a normal distribution, and the multiply them in order to approximate the rbf kernel
         self.W = np.sqrt(2 * self.gamma) * np.random.randn(self.D, d)
+
+        #generate random bias from 0 to 2pi
         self.b = 2 * np.pi * np.random.rand(self.D)
 
     def transform(self, X):
+        #transform original data in the Random Fourier space, each column represents a random projection of the original data
         projection = X @ self.W.T + self.b
+        # z(x) = sqrt(2/D) * cos(Wx + b) is the formula for the random fourier featuers, np.sqrt(2.0 / self.D) this is a normalization factor
         return np.sqrt(2.0 / self.D) * np.cos(projection)
 
     def fit_transform(self, X):
@@ -23,33 +33,47 @@ class RandomFourierFeatures:
 
 class RandomBinningFeatures:
     def __init__(self, D=100, gamma=1.0, buckets=8):
-        self.D = D
-        self.gamma = gamma
-        self.buckets = buckets
+        self.D = D #number of random partitions generated
+        self.gamma = gamma # controls the mean dimensions of the cell
+        self.buckets = buckets # number of hashed buckets for each cells, need to control the final dimensions
 
     def fit(self, X):
+        #number of initial features
         d = X.shape[1]
 
+        #Build a matrix (D,d), every row represent a different random grid, with random numbers genrated from an exponential distribution.
+        #Scaled by 1/gamma
+        #If gamma increase, delta becames smaller the cells are smaller and so we have smaller partitions
+        #If gamma deacres, delta becames greater the cells are bigger and so we have bigger partitions
         self.delta = np.random.exponential(scale=1.0 / self.gamma,size=(self.D, d))
+        #In order to avoid division by 0 or really small values
         self.delta = np.maximum(self.delta, 1e-8)
-
+        #Generate a random offset for each cells in order to random move the cells in the space, in this way they are not aligned with the same input
         self.offset = np.random.uniform(0, self.delta)
-
+        #Generate random weights, need to create an hash function that converts coordinates into a single discrete index
         self.hash_weights = np.random.randint(1,10_000,size=(self.D, d))
 
     def transform(self, X):
+        #number of samples
         n = X.shape[0]
+        #final matrix of features
         Z = np.zeros((n, self.D * self.buckets))
 
+        #iterates on the random grids
         for j in range(self.D):
+            #calculate the bins were the points fell
             bins = np.floor((X + self.offset[j]) / self.delta[j]).astype(int)
+            #convert the coordinates into a single hashed bucket
             hashed = np.abs(bins @ self.hash_weights[j]) % self.buckets
 
+            #each sample activate ONE feature for each grid
             rows = np.arange(n)
             cols = j * self.buckets + hashed
 
+            #one hot encoding, set to 1 the activated feature
             Z[rows, cols] = 1.0
 
+        #normalizations
         return Z / np.sqrt(self.D)
 
     def fit_transform(self, X):
@@ -59,30 +83,21 @@ class RandomBinningFeatures:
 
 class Perceptron:
     def __init__(self, lr=0.001, epochs=1000, feature_map="linear", D=100, gamma=1.0, buckets=32):
-        """
-        Perceptron binario from scratch.
 
-        feature_map:
-        - "linear"          -> Perceptron lineare classico
-        - "rff"             -> Perceptron con Random Fourier Features
-        - "random_binning"  -> Perceptron con Random Binning Features
-        """
+        self.lr = lr #learning rate
+        self.epochs = epochs #epochs of training
+        self.feature_map = feature_map #select the feature mapping
+        self.D = D #number of finite dimensione if the mapping is not linear
+        self.gamma = gamma #parameter if the mapping is not linear
+        self.buckets = buckets #number of buckets if the mapping is not linear
 
-        self.lr = lr
-        self.epochs = epochs
-        self.feature_map = feature_map
-        self.D = D
-        self.gamma = gamma
-        self.buckets = buckets
-
-        self.mapper = None
-        self.w = None
-        self.b = None
+        self.mapper = None #save the mapping to use
+        self.w = None #weights
+        self.b = None #bias
 
     def _init_feature_map(self, X):
-        """
-        Inizializza e applica la trasformazione scelta sul training set.
-        """
+
+        #initialize and apply the transormation choosen on the training set
 
         if self.feature_map == "linear":
             self.mapper = None
@@ -99,163 +114,136 @@ class Perceptron:
         raise ValueError("feature_map deve essere: 'linear', 'rff' oppure 'random_binning'")
 
     def _transform(self, X):
-        """
-        Applica al test set la stessa trasformazione imparata sul training set.
-
-        Importante:
-        - sul training set si usa fit_transform()
-        - sul test set si usa solo transform()
-
-        Così le random features restano le stesse.
-        """
-
         if self.mapper is None:
             return X
 
         return self.mapper.transform(X)
 
     def fit(self, X, y):
-        """
-        Allena il Perceptron.
-
-        Il training avviene sempre in modo lineare, ma lo spazio cambia:
-
-        - linear:
-            f(x) = w^T x + b
-
-        - rff:
-            f(x) = w^T z_RFF(x) + b
-
-        - random_binning:
-            f(x) = w^T z_RB(x) + b
-        """
-
+        #number of samples
         n_samples = X.shape[0]
-
+        #apply the choosen mapping
         X_train = self._init_feature_map(X)
-
+        #number of features
         n_features = X_train.shape[1]
 
+        #init bias and weights
         self.w = np.zeros(n_features)
         self.b = 0.0
 
+        #convert label
         y_signed = np.where(y == 0, -1, 1)
 
+        #iterate over epochs
         for _ in range(self.epochs):
-
+            #permute the indices at each iteration
             indices = np.random.permutation(n_samples)
 
+            #iterate over the samples
             for i in indices:
-
+                #calculate the score = w^T x_i + b
                 score = X_train[i] @ self.w + self.b
-
+                #calculate the margin, if is positive the sample is correctly classified, if it is <= 0 the point is misclassified or on the decision boundaries
                 margin = y_signed[i] * score
-
+                
                 if margin <= 0:
+                    #update parameters if the point is misclassified
                     self.w += self.lr * y_signed[i] * X_train[i]
                     self.b += self.lr * y_signed[i]
 
         return self
 
     def decision_function(self, X):
-        """
-        Calcola lo score del modello:
-
-            f(x) = w^T phi(x) + b
-
-        dove phi(x) può essere:
-        - identità
-        - Random Fourier Features
-        - Random Binning Features
-        """
-
+        #calculate the score of the model f(x) = w^T phi(x) + b, phi(x) is the choosen transformation
         X_transformed = self._transform(X)
         return X_transformed @ self.w + self.b
 
     def predict(self, X):
-        """
-        Predice label in formato {0, 1}.
-        """
-
+        #if scores is grater or equal to 0 it predicts 1, otherwise 0
         scores = self.decision_function(X)
         return np.where(scores >= 0, 1, 0)
 
 
 class KernelSVM:
     def __init__(self, lr=0.001, epochs=1000, C=1.0, gamma=1.0, kernel="rbf"):
-        """
-        SVM kernelizzata from scratch usando il problema duale.
 
-        kernel:
-        - "rbf"     -> K(x, z) = exp(-gamma * ||x - z||^2)
-        - "laplace" -> K(x, z) = exp(-gamma * ||x - z||_1)
-        """
+        self.lr = lr #learning rate
+        self.epochs = epochs #train epochs
+        self.C = C #regularization paremeter, if it is high, penalize more the error, if it is low penaize less the error
+        self.gamma = gamma #parameter of the kernel, control how much fast deacrese the similarity between points
+        self.kernel = kernel #selected kernel
 
-        self.lr = lr
-        self.epochs = epochs
-        self.C = C
-        self.gamma = gamma
-        self.kernel = kernel
-
-        self.alpha = None
-        self.b = 0.0
-        self.X_train = None
-        self.y_train = None
+        self.alpha = None #coefficients of the dual SVM, a coefficient for each sample
+        self.b = 0.0 #bias 
+        self.X_train = None #train set sample
+        self.y_train = None #train set label
 
     def _kernel_function(self, X1, X2):
-        """
-        Calcola la matrice kernel tra X1 e X2.
-        """
-
+        
+        #Kernel RBF/Gaussiano: K(x,z) = exp(-gamma * ||x - z||^2)
         if self.kernel == "rbf":
             return kernels.rbf_kernel(X1, X2, gamma=self.gamma)
 
+        #Kernel Laplaciano: K(x,z) = exp(-gamma * ||x - z||_1)
         if self.kernel == "laplace":
             return kernels.laplace_kernel(X1, X2, gamma=self.gamma)
 
         raise ValueError("kernel deve essere 'rbf' oppure 'laplace'")
 
     def fit(self, X, y):
-        """
-        Allena la SVM kernelizzata.
-        """
-
+        #number of samples
         n_samples = X.shape[0]
-
+        #convert the label
         y_signed = np.where(y == 0, -1, 1)
 
         self.X_train = X
         self.y_train = y_signed
 
+        #calcolate the gram matrix, containing the transfromations of the kernell, K[i, j] = K(x_i, x_j)
+        #contain all the similarity between points
         K = self._kernel_function(X, X)
 
+        #matrix: Q_ij = y_i * y_j * K(x_i, x_j)
         Q = np.outer(y_signed, y_signed) * K
 
+        #set coefficients for each samples
         self.alpha = np.zeros(n_samples)
 
+        #training epochs
+        #maximize this: sum_i alpha_i - 1/2 sum_i sum_j alpha_i alpha_j y_i y_j K(x_i,x_j)
         for _ in range(self.epochs):
-
+            #Gradient w.r.t alpha, if is positive alpha tends to get higher, otherwise not
             grad = 1 - Q @ self.alpha
-
+            #upgrade coefficients with gradient ascent
             self.alpha += self.lr * grad
-
+            #force each alpha between 0 and C
             self.alpha = np.clip(self.alpha, 0, self.C)
 
+            #correction in order to have that sum_i alpha_i y_i = 0
             correction = np.dot(self.alpha, y_signed) / np.sum(y_signed ** 2)
             self.alpha -= correction * y_signed
 
+            #after the update some alphas may be violated the constraints, so we have to force again
             self.alpha = np.clip(self.alpha, 0, self.C)
 
+        #identify the support vectors using point with a_i between 0 and C
         support_vector_mask = (self.alpha > 1e-5) & (self.alpha < self.C - 1e-5)
-
+        
+        
+        #for any support vector we have y_i = sum_j alpha_j y_j K(x_j, x_i) + b and so b = y_i - sum_j alpha_j y_j K(x_j, x_i)
         if np.any(support_vector_mask):
+            #indices of support vectors
             support_indices = np.where(support_vector_mask)[0]
             b_values = []
 
+
             for i in support_indices:
+                #decision function withoout considering the bias
                 decision_without_b = np.sum(self.alpha * y_signed * K[:, i])
+                #value of b using support vector i
                 b_values.append(y_signed[i] - decision_without_b)
 
+            #mean of the biases
             self.b = np.mean(b_values)
         else:
             self.b = 0.0
@@ -263,17 +251,16 @@ class KernelSVM:
         return self
 
     def decision_function(self, X):
-        """
-        f(x) = sum_i alpha_i y_i K(x_i, x) + b
-        """
 
+
+        #calcuate kernel matrix
         K = self._kernel_function(X, self.X_train)
+        #each prediction use all the training points weighetd by alpha_i * y_i
+        #in practice we use the support vectors beacuse alphas close to zero have a small impact
         return K @ (self.alpha * self.y_train) + self.b
 
     def predict(self, X):
-        """
-        Predice label in formato {0, 1}.
-        """
-
+        
+        #calculate the scores
         scores = self.decision_function(X)
-        return np.where(scores >= 0, 1, 0)
+        return np.where(scores >= 0, 1, 0) #if the score >= 0 then the prediction is 1, else 0
